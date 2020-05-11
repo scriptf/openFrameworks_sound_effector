1a321f4eb73b2b7ad820a31eebf78943f510e25f
xxx <xxx@local>
2020-05-11 17:29:52 +0900

    Add gui

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "ofMain.h"
+//#include "ofxGui.h"
+#include "ofxDatGui.h"
+
 
 class ofApp : public ofBaseApp{
 	
@@ -45,5 +48,29 @@ class ofApp : public ofBaseApp{
         vector <float> lAudio;
         vector <float> rAudio;
         float     volume;
+        
+        //--------- gui
+        /*
+        ofxPanel gui;
+        ofxFloatSlider volume;
+        bool bHide;
+        */
+        ofxDatGui* gui;
+        bool mFullscreen;
+        void refreshWindow();
+        void toggleFullscreen();
+
+        void onButtonEvent(ofxDatGuiButtonEvent e);
+        void onToggleEvent(ofxDatGuiToggleEvent e);
+        void onSliderEvent(ofxDatGuiSliderEvent e);
+        void onTextInputEvent(ofxDatGuiTextInputEvent e);
+        void on2dPadEvent(ofxDatGui2dPadEvent e);
+        void onDropdownEvent(ofxDatGuiDropdownEvent e);
+        void onColorPickerEvent(ofxDatGuiColorPickerEvent e);
+        void onMatrixEvent(ofxDatGuiMatrixEvent e);
+    
+        uint tIndex;
+        vector<ofxDatGuiTheme*> themes;
 
 };
+    
